801c77ff9ef25c34c2d4811b421c02acbb9d6c3b
Iremiwa <somoayo@yahoo.com>
2019-11-14 16:52:44 +0100

    Updates

---
+++ precache-manifest.9da6f6401b869ceb17e8366df915fd87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fd61ca0edb431d3aa611c1afbb9336",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "4900807addbc48d4f50f",
+    "url": "/todo-list/static/css/main.8ee68052.chunk.css"
+  },
+  {
+    "revision": "7d550c2f15659df9399a",
+    "url": "/todo-list/static/js/2.75f37552.chunk.js"
+  },
+  {
+    "revision": "4900807addbc48d4f50f",
+    "url": "/todo-list/static/js/main.545bfb3a.chunk.js"
+  },
+  {
+    "revision": "f93898e5356ea7cd0a6f",
+    "url": "/todo-list/static/js/runtime-main.7417797d.js"
+  }
+]);
